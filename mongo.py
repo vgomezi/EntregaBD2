7d26daa57fa1db9c8c5b9d33120ac27ef88886a3
mvdelcampo <105162990+mvdelcampo@users.noreply.github.com>
2022-11-14 22:28:09 -0300

    ok

--- mongo.py
+++ mongo.py
@@ -1,6 +1,9 @@
+from datetime import date
 from pymongo import MongoClient
 
-def get_database():
+from Funciona_EntregaORM import Cliente
+
+def get_database_collection():
  
    # Provide the mongodb atlas url to connect python to mongodb using pymongo
    CONNECTION_STRING = "mongodb://localhost:27017/"
@@ -10,9 +13,42 @@ def get_database():
  
    # Create the database for our example (we will use the same database throughout the tutorial
    return client.mdbg2.pedidos
+
+def alta_pedido_simple_m(dni_cliente_i, precio_total_i, estado_i, fecha_obj_i, canal_compra_i, nro_pedido_compuesto_i):
+
+    print("hola1")
+    # Get the database
+    pedidos = get_database_collection()
+    print("hola2")
+
+    #if Cliente.select().where(Cliente.dni == dni_cliente_i).exists():
+   
+    if nro_pedido_compuesto_i:
+        pass
+    else:
+        print("hola")
+
+        item = {
+            #"_id" : "U1IT00001",
+            "precio_total" : precio_total_i,
+            "estado" : estado_i,
+            "fecha" : fecha_obj_i,
+            "canal_compra" : canal_compra_i,
+            "nro_pedido_compuesto" : None,
+            "dni_cliente" : dni_cliente_i 
+        }
+
+        pedidos.insert_one(item)
+
   
 # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":   
-  
-   # Get the database
-   dbname = get_database()
+
+    print("hola0")
+    alta_pedido_simple_m(12345678,20,'pendiente', date.today(), 'web', None)
+
+
+
+    #else:
+    #    print ("Error: cliente no existe")
+   
