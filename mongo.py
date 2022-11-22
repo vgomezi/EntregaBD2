7631d1b16256b81b0d55e90d47a06948bbe70ba9
mvdelcampo <105162990+mvdelcampo@users.noreply.github.com>
2022-11-21 21:33:15 -0300

    ok

--- mongo.py
+++ mongo.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 from Funciona_EntregaORM import Cliente
 
-'''
+
 def get_database():
  
 #Provide the mongodb atlas url to connect python to mongodb using pymongo
@@ -54,13 +54,13 @@ def buscar_pedido_simple_id_m(id_buscar):
    pedidos = mymongo.get_collection('Pedidos')
 
    pedidos.find(
-      {{"_id" : id_buscar}
-      {"precio_total" : 1,
-      "estado" : 1,
-      "fecha" : 1,
-      "canal_compra" : 1,
-      "nro_pedido_compuesto" :1,
-      "dni_cliente" 1}}
+      {"_id" : id_buscar},
+      {"precio_total": 1,
+      "estado": 1,
+      "fecha": 1,
+      "canal_compra": 1,
+      "nro_pedido_compuesto":1,
+      "dni_cliente": 1}
    )
 
 
@@ -69,18 +69,16 @@ def buscar_pedido_compuesto_id_m(id_buscar):
    pedidos = mymongo.get_collection('Pedidos')
 
    pedidos.find(
-      {{"_id" : id_buscar}
-      {"fecha" : 1,
-      "canal_compra" : 1,
-      "dni_cliente" 1}}
+      {'_id' : id_buscar},
+      {'fecha': 1,
+      'canal_compra': 1,
+      'dni_cliente': 1}
    )
-
 
 if __name__ == '__main__':   
 
    mymongo = get_database()
    
-   #alta_pedido_simple_m(12345678, 30,'listo', date.today(), 'movil', 1)
-   alta_pedido_compuesto_m( date.today(), 'movil', 12345678)
+   alta_pedido_simple_m(11111111, 60,'pendiente', date.today(), 'web', 1)
+   alta_pedido_compuesto_m( date.today(), 'web', 11111111)
 
-'''
